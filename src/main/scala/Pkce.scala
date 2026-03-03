9ecd5508e4e27ffde5938fa450626c77251434b9
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2026-03-02 23:14:16 -0500

    cleanup pkce util

--- src/main/scala/Pkce.scala
+++ src/main/scala/Pkce.scala
@@ -2,16 +2,13 @@ import java.security.{ MessageDigest, SecureRandom }
 import java.util.Base64
 
 object PKCEUtil:
+  private val random = new SecureRandom()
+
   def generateCodeVerifier(): String =
-    val secureRandom = new SecureRandom()
     val bytes = new Array[Byte](32)
-    secureRandom.nextBytes(bytes)
+    random.nextBytes(bytes)
     Base64.getUrlEncoder.withoutPadding().encodeToString(bytes)
 
   def generateCodeChallenge(verifier: String): String =
-    val sha256 = MessageDigest.getInstance("SHA-256")
-    val digest = sha256.digest(verifier.getBytes("UTF-8"))
+    val digest = MessageDigest.getInstance("SHA-256").digest(verifier.getBytes("UTF-8"))
     Base64.getUrlEncoder.withoutPadding().encodeToString(digest)
-
-  def verifyCode(verifier: String, challenge: String): Boolean =
-    generateCodeChallenge(verifier) == challenge
